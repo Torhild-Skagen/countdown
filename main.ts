8f6674390a6442ff3a16d23e37295a37b50a3931
Torhild-Skagen <85964199+Torhild-Skagen@users.noreply.github.com>
2021-09-12 22:24:36 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,6 @@
+for (let indeks = 0; indeks <= 2; indeks++) {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    basic.showNumber(3 - indeks)
+}
+music.playTone(392, music.beat(BeatFraction.Whole))
+basic.showString("GO")
